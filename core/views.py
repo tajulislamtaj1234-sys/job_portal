bd6502c4d71334dce090e004369bc9286fa024b8
tazul islam <tajulislamtaj1234@gmail.com>
2026-07-02 00:13:37 +0900

    feat: add dynamic badge colors using Django template if

--- core/views.py
+++ core/views.py
@@ -1,8 +1,55 @@
 from django.shortcuts import render
 from django.views import View
 
-class HomeView(View):
-    def get(self, request):
-        return render(request,'home.html')
 
-# Create your views here.
+class HomeView(View):
+
+    def get(self, request):
+
+        jobs = [
+
+            {
+                "title": "Python Django Developer",
+                "company": "Google",
+                "location": "Tokyo, Japan",
+                "type": "Full Time",
+            },
+
+            {
+                "title": "Frontend Developer",
+                "company": "Microsoft",
+                "location": "Osaka, Japan",
+                "type": "Remote",
+            },
+
+            {
+                "title": "Backend Developer",
+                "company": "C.A.L",
+                "location": "Minato, Japan",
+                "type": "Part Time",
+            },
+
+            {
+                "title": "DevOps Engineer",
+                "company": "Amazon",
+                "location": "Tokyo",
+                "type": "Contract",
+            },
+
+            {
+                "title": "AI Engineer",
+                "company": "OpenAI Japan",
+                "location": "Kyoto",
+                "type": "Internship",
+            }
+
+
+        ]
+
+        context = {
+
+            "jobs": jobs,
+
+        }
+
+        return render(request, "home.html", context)
